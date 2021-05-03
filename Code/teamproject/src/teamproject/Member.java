a4e6cd6acaab940531f8c63a529077a10cb8c291
박상준 <qkr03210@gmail.com>
2021-05-03 17:46:10 +0900

    1차 기본 베이스

---
+++ Code/teamproject/src/teamproject/Member.java
@@ -0,0 +1,36 @@
+package teamproject;
+
+public class Member {
+	private String id;
+	private String pw;
+	private String name;
+	private String phone;
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", pw=" + pw + ", name=" + name + ", phone=" + phone + "]";
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
